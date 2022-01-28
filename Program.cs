61f80132f5a0163eed59686af1939da0b52cf63b
manny <manny@TOMMY-OLIVER>
2022-01-28 14:31:44 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Assignment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
