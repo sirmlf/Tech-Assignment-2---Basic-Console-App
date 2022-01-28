749d216c2156598b6a744d2206dfe5fb27e1adfe
Manuel Fernandez <manuelfernan@usf.edu>
2022-01-28 14:50:44 -0500

    Commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿// Tech Assignment 2 Basic Console App
+// Submitted by Manuel Fernandez
+
+using System;
 
 namespace Tech_Assignment_2___Basic_Console_App
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This quote always gets me thinking about the state of the world");
+            Console.WriteLine("'Wages are the weights that hold us down from following out actual dreams'");
+            Console.WriteLine("Submitted by Manuel Fernandez");
         }
     }
 }
